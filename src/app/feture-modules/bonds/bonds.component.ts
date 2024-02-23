9c30102457a2ea26ee6a89607d8a4c1bef296a45
szymon_p <szymon.polanski24@gmail.com>
2024-02-23 14:34:16 +0100

    application setup

---
+++ src/app/feture-modules/bonds/bonds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonds',
+  standalone: true,
+  imports: [],
+  templateUrl: './bonds.component.html',
+  styleUrl: './bonds.component.scss'
+})
+export class BondsComponent {
+
+}
